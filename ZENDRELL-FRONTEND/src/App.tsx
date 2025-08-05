1685767cb10380649ed932c38ea824e7f20bf56b
chiamaka-igbokwe <chiamakaigbokwex@gmail.com>
2025-08-05 21:13:08 +0100

    new commit

---
+++ ZENDRELL-FRONTEND/src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="min-h-screen bg-yellow-400 flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-center text-black">
+        Hello, Vite + React + TypeScript + Tailwind!
+      </h1>
+      <button className="bg-red-500">Hi</button>
+    </div>
+  );
+};
+
+export default App;
